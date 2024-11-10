053b22ca407276a69b4a281134a6fe07acae73d7
dhlananhh <21087481.anh@student.iuh.edu.vn>
2024-11-10 13:47:56 +0700

    xóa folder `redux-toolkit`

--- redux-toolkit/sagas.js
+++
@@ -1,8 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import todoSaga from './todoSaga'; // We will create this file later
-
-export default function* rootSaga() {
-    yield all([
-        fork(todoSaga),
-    ]);
-}
--- redux-toolkit/store.js
+++
@@ -1,9 +0,0 @@
-// redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import bikeShopReducer from './bikeShopSlice'; // Create this slice
-
-export const store = configureStore({
-  reducer: {
-    bikes: bikeShopReducer, // Use your bikeShopReducer here
-  },
-});
