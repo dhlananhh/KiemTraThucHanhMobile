1a566c0f7aa3f904c1a88cd5ef8fef07d088bb2b
dhlananhh <21087481.anh@student.iuh.edu.vn>
2024-11-10 13:49:49 +0700

    cập nhật file `App.js`

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
-import Screen01 from './components/Screen01'
+import Screen01 from './components/Screen01';
 import Screen02 from './components/Screen02';
 import Screen03 from './components/Screen03';
 
-
 const Stack = createStackNavigator();
-
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Screen01" component={Screen01} />
-        <Stack.Screen name="Screen02" component={Screen02} />
-        <Stack.Screen name="Screen03" component={Screen03} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name="Screen01" component={Screen01} />
+          <Stack.Screen name="Screen02" component={Screen02} />
+          <Stack.Screen name="Screen03" component={Screen03} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
